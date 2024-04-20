2bf79cdc1eb35f7567df7cba1825173b9594a663
noname12324343 <108060942+noname12324343@users.noreply.github.com>
2024-04-20 21:21:04 +0700

    Add files via upload
    Code này dùng để tạo nhanh ví TON
    Bản quyền thuộc về https://t.me/Justice0210
    Ngoài ra mình bán tool check live Twitter (X), Tool đọc mail,
    ae nào cần có thể ủng hộ mình :3

---
+++ Ton Wallet.py
@@ -0,0 +1,31 @@
+from tonsdk.contract.wallet import WalletVersionEnum, Wallets
+from tonsdk.utils import bytes_to_b64str
+from tonsdk.crypto import mnemonic_new
+from colorama import init, Fore, Back, Style
+
+init()
+
+print(Fore.GREEN + """                      Bản quyền thuộc về https://t.me/Justice0210
+            Ngoài ra mình bán tool check live Twitter (X), Tool đọc mail,
+                            ae nào cần có thể ủng hộ mình :3""" + Style.RESET_ALL)
+
+wallet_workchain = 0
+wallet_version = WalletVersionEnum.v3r2
+f = open('Ton Wallet.txt','a')
+quantity = int(input('Nhập số ví cần tạo: '))
+for i in range(quantity):
+    wallet_mnemonics = mnemonic_new()
+
+    _mnemonics, _pub_k, _priv_k, wallet = Wallets.from_mnemonics(
+        wallet_mnemonics, wallet_version, wallet_workchain)
+    query = wallet.create_init_external_message()
+    base64_boc = bytes_to_b64str(query["message"].to_boc(False))
+    address = wallet.address.to_string(True, True, False)
+    print(f"""
+    {i + 1}
+    Mnemonic: {' '.join(wallet_mnemonics)}
+    Bounceable, url safe, user friendly address: {address}
+
+    """)
+    f.write(f'{address} | {" ".join(wallet_mnemonics)}\n')
+
